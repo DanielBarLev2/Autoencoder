c3461b1d025c954e92837fc50d4f6b2d77043bd5
danielbarlev2 <danielbl2010@gmail.com>
2024-11-20 11:49:33 +0200

    dataloader

---
+++ main.py
@@ -0,0 +1,6 @@
+from src import model
+
+if __name__ == '__main__':
+    print("*******Connected to server*******")
+    model()
+    print("*******Disconected from server*******")
---
+++ src/model.py
@@ -0,0 +1,7 @@
+import data_loader.load_dataset
+
+dataset_path = "/home/ML_courses/03683533_2024/dataset"
+
+val_imgs, train_imgs = load_dataset(dataset_path)
+
+print("done")
